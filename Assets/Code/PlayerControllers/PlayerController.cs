a6ae09c4b278dedb9d9d265735dcc66409ae23b3
shahabsy <shahabsy@hotmail.com>
2026-03-27 16:17:22 +0500

    update player prefab for 2d and host and remote / client player colors.

--- Assets/Code/PlayerControllers/PlayerController.cs
+++ Assets/Code/PlayerControllers/PlayerController.cs
@@ -2,11 +2,14 @@ using Unity.Netcode.Components;
 using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.InputSystem;
+using System.Globalization;
 
 public class PlayerController : NetworkBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
     private NetworkTransform _networkTransform;
+
+    private Vector2 _moveInput;
 
     private void Awake()
     {
@@ -14,6 +17,25 @@ public class PlayerController : NetworkBehaviour
         if ( _networkTransform == null )
         {
             _networkTransform = gameObject.AddComponent<NetworkTransform>();
+        }
+        Debug.Log($"PlayerController Awake on {gameObject.name}, IsOwner: {IsOwner}, IsSpawned {IsSpawned}");
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        Debug.Log($"Player spawned! ClientId: {OwnerClientId}, IsOwner: {IsOwner}, IsServer: {IsServer}, IsClient: {IsClient}");
+
+        if (IsOwner)
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+            Debug.Log("This is my player (owner)");
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+            Debug.Log("This is a Remote player");
         }
     }
 
